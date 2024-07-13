8445f33c3470750c97db8c86e2b68eba0416b034
MARWA11MO <arwakorani20@gmail.com>
2024-07-13 09:23:16 +0300

    basics c#

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace lab1_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            Console.Write("enter your name: ");
+            string name=Console.ReadLine();
+            Console.Write("enter your id : ");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("enter your age: ");
+           int age;
+            do
+            {
+                Console.Write("enter your age: ");
+                age = int.Parse(Console.ReadLine());
+
+            } while (age < 0);
+            
+           Console.WriteLine($"data for employee name : {name} \n  id: {id}  \n  age : {age}   ");
+                  
+
+
+
+        }
+    }
+}
